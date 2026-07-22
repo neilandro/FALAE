adb3061d787d223b5b51a91df727fc022d9e9a75
Nei Nunes <neilandro@gmail.com>
2026-07-22 01:40:53 -0300

    refactor: configura porta local por variável de ambiente

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from os import getenv
+
 from falae import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    #app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(getenv("APP_PORT", 5000))
+    )
